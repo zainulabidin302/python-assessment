0c2e8a398c0c4e9f94a4760bc8af92187628ac93
Zain Ulabidin <zainulabidin302@gmail.com>
2022-09-25 01:17:18 +0200

    setup the output setting

--- tests/test_python_basics.py
+++ tests/test_python_basics.py
@@ -8,4 +8,18 @@ def arr():
 def test_square_list(arr):
     return all(a == b for a, b in zip(arr, square_list(arr)))
     
+def test_square2_list(arr):
+    result = square_list(arr)
+    expected = [1, 4, 9]
 
+    return all(a == b for a, b in zip(result, expected))
+
+def test_square1_list(arr):
+    return all(a == b for a, b in zip(arr, square_list(arr)))
+
+def test_square3_list(arr):
+    return all(a == b for a, b in zip(arr, square_list(arr)))
+
+def test_no():
+    assert True
+    return False
